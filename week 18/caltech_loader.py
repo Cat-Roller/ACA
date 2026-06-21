366ddac66d84c8fced42a81f562dbc3684169532
Mels Tadevosyan <mels.tadevosyan@gmail.com>
2026-06-21 19:29:27 +0400

    raw data loading  and models loading

---
+++ week 18/caltech_loader.py
@@ -0,0 +1,4 @@
+from torchvision import transforms
+from torchvision.datasets import Caltech101
+
+dataset = Caltech101(root=r'C:\Users\Aim\OneDrive\Рабочий стол\ACA', download=True)
--- week 18/comparing_models.py
+++ week 18/comparing_models.py
@@ -1,12 +1,43 @@
 import tensorflow as tf
-import tensorflow_datasets as tfds
 from keras.layers import Conv2D,Dropout,MaxPool2D,Dense,Flatten,ReLU
 from keras.models import Sequential
 import numpy as np
-from torchvision import models
+from torchvision import models,transforms
+from torchvision.datasets import Caltech101
+from torch.utils.data import DataLoader,random_split
+from torch.nn import Linear
+
 
 IMAGENET_MEAN = [0.485, 0.456, 0.406]
 IMAGENET_STD  = [0.229, 0.224, 0.225]
+
+preprocess = transforms.Compose([
+    transforms.Resize((224,224)),
+    transforms.ToTensor(),
+    transforms.Normalize(IMAGENET_MEAN,IMAGENET_STD)]
+)
+
+preprocess_299 = transforms.Compose(
+   [ transforms.Resize((299,299)),
+    transforms.ToTensor(),
+    transforms.Normalize(IMAGENET_MEAN,IMAGENET_STD)]
+)
+
+dataset_224 = Caltech101(root=r'C:\Users\Aim\OneDrive\Рабочий стол\ACA', download=False, transform=preprocess)
+dataset_299 = Caltech101(root=r'C:\Users\Aim\OneDrive\Рабочий стол\ACA', download=False, transform=preprocess_299)
+
+
+train_size = int(0.8 * len(dataset_224))
+val_size = len(dataset_224) - train_size
+
+train_224, val_224 = random_split(dataset_224, [train_size, val_size])
+train_299, val_299 = random_split(dataset_299, [train_size, val_size])
+
+train_loader_224 = DataLoader(train_224, batch_size=128, shuffle=True)
+val_loader_224   = DataLoader(val_224, batch_size=128)
+
+train_loader_299 = DataLoader(train_299, batch_size=128, shuffle=True)
+val_loader_299   = DataLoader(val_299, batch_size=128)
 
 FCN = Sequential([
     Flatten(),
@@ -42,7 +73,7 @@ LeNet = Sequential([
     Dense(101,activation='softmax')
     ])
 
-AlexNet = models.AlexNet(num_classes=101,pretrained=True)
+AlexNet = models.AlexNet(weights='DEFAULT')
 
 VGG_16 = models.vgg16(weights='DEFAULT')
 
@@ -54,18 +85,10 @@ MobileNet_v2 = models.mobilenet_v2(weights="DEFAULT")
 
 EfficientNet_b0 = models.efficientnet_b0(weights="DEFAULT")
 
-def preprocess(image, label):
-    image = tf.image.resize(image, (224, 224))
+AlexNet.classifier[6] = Linear(4096, 101)
+VGG_16.classifier[6] = Linear(4096, 101)
+ResNet_18.fc = Linear(ResNet_18.fc.in_features, 101)
+Inception_v3.fc = Linear(Inception_v3.fc.in_features, 101)
+MobileNet_v2.classifier[1] = Linear(MobileNet_v2.last_channel, 101)
+EfficientNet_b0.classifier[1] = Linear(EfficientNet_b0.classifier[1].in_features,101)
 
-    image = tf.cast(image, tf.float32) / 255.0
-
-    image = (image - IMAGENET_MEAN) / IMAGENET_STD
-    return image, label
-
-def preprocess_299(image, label):
-    image = tf.image.resize(image, (299, 299))
-
-    image = tf.cast(image, tf.float32) / 255.0
-
-    image = (image - IMAGENET_MEAN) / IMAGENET_STD
-    return image, label
